933056bb9febc66f134c7a691b6213d2d7d210a8
KwatME <kwat.me@icloud.com>
2021-04-06 15:04:20 -0700

    Google

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,18 +63,22 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: [
-            "UA-186228916-3",
-            "GA-268104161",
-        ],
+        trackingIds: ["G-9YRYMLE5CB", "2412434867"],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
         gtagConfig: {
           optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
         },
+        // This object is used for configuration specific to this plugin
         pluginConfig: {
+          // Puts tracking script in the head instead of the body
           head: false,
+          // Setting this parameter is also optional
           respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
         },
       },
     },
