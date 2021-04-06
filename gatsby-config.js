c33f910ac06e8ed31b131cf40884502a5672fd94
KwatME <kwat.me@icloud.com>
2021-04-06 16:01:46 -0700

    No point crying over spilled milk

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-186228916-4",
+        trackingId: "UA-186228916-1",
         head: true,
       },
     },
